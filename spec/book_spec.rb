5e3dd5b3074291c7c788eb97e4044e1c0a281e0b
Godwin Nwachukwu <nwgwin@gmail.com>
2022-07-16 20:58:49 +0100

    finish testing

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -24,11 +24,6 @@ describe Book do
       @book_to_test.should be_an_instance_of Book
     end
 
-    it 'Tests to see if the book can be archived - but returns false when needed' do
-      @book_to_test.publish_date = DateTime.parse('2022-01-01')
-      expect(@book_to_test.can_be_archived?).to eq false
-    end
-
     it 'Tests to make sure our test book becomes the correct format in order to store between sessions' do
       actual = { title: 'Game of Thrones', publish_date: '2000-01-01', publisher: 'GoT Publishing', cover_state: 'Good',
                  author: { first_name: 'George', last_name: 'Martin', id: 33 }, label: { title: 'Fantasy', color: 'Purple', id: 33 }, archived: false, id: 33 }
