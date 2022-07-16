922846ca9cf63bf3d7da1130662b44f5c6db6a7d
Godwin Nwachukwu <nwgwin@gmail.com>
2022-07-16 14:47:43 +0100

    refactor

--- lib/item.rb
+++ lib/item.rb
@@ -25,21 +25,21 @@ class Item
 
   def add_genre(genre)
     @genre = genre
-    # genre.items.push(self)
+    genre.items.push(self) if defined?(@genre.items) && !genre.items.include?(self)
   end
 
   def add_author(author)
     @author = author
-    # author.items.push(self)
+    author.items.push(self) if defined?(@author.items) && !author.items.include?(self)
   end
 
   def add_source(source)
     @source = source
-    # source.items.push(self)
+    source.items.push(self) if defined?(@source.items) && !source.items.include?(self)
   end
 
   def add_label(label)
     @label = label
-    # label.items.push(self)
+    label.items.push(self) if defined?(@label.items) && !label.items.include?(self)
   end
 end
